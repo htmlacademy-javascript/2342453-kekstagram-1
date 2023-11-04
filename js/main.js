7af4ef20818856086f008030d3d154efb2cca45c
PeshiyLeshiy <126255481+PeshiyLeshiy@users.noreply.github.com>
2023-11-04 20:09:12 +0500

    ДЗ к разделу №7. Отрисуй меня полностью.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,4 @@
-import {getPictures} from './data.js';
-
-getPictures();
-// Импортировал и вызвал.
-
 import { getPictures } from './data.js';
 import { renderPictures } from './picture.js';
-import './form.js';
 
 renderPictures(getPictures());
